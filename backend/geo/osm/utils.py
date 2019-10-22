1f2c0018786872ad85a2a98dc2a56a6659ec58e7
colykxer <colykxer@gmail.com>
2019-10-22 23:19:54 +0200

    Add public transport api

--- backend/geo/osm/utils.py
+++ backend/geo/osm/utils.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from typing import Dict, Union
 
 import requests
@@ -13,6 +14,18 @@ def get_info_by_coord(lat: float, lon: float) -> Union[Dict, None]:
     return None
 
 
+def get_bus_station_by_coord(lat: float, lon: float) -> Union[Dict, None]:
+    url = "http://transit.land/api/v1/stops"
+    params = {"lat": lat, "lon": lon}
+    res = requests.get(url, params)
+    if res.ok:
+        return res.json()
+
+    return None
+
+
 if __name__ == "__main__":
     info = get_info_by_coord(51.21824, 22.4233422)
+    bus_info = get_bus_station_by_coord(51.23997, 22.5515647)
     print(info)
+    pprint(bus_info)
