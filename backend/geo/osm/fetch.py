b8a3707c268357521191abc06b2060eda94c8e3c
digitalrebel78 <voytroman28@gmail.com>
2019-10-23 00:28:12 +0200

    Add multi-tag fetch. Add common way types.

--- backend/geo/osm/fetch.py
+++ backend/geo/osm/fetch.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import json
 import overpass
 
@@ -7,11 +5,13 @@ api = overpass.API()
 
 
 def fetch_data_by_id(area_id, ref_class, el_class, el_type, responseformat):
-    query = ref_class + '(' + str(area_id) + ');\nmap_to_area;\n' + el_class + '(area)'
+    query = ref_class + '(' + str(area_id) + ');\nmap_to_area->.a;\n(\n'
     selector = type_to_selector(el_type)
     if selector is None:
         return None
-    query += selector + ';\n(._;>;);\nout;'
+    for val in selector:
+        query += el_class + '(area .a)' + val + ';\n'
+    query += ');\n(._;>;);\nout;'
     return api.get(query, responseformat=responseformat)
 
 
@@ -34,14 +34,42 @@ def type_to_selector(el_type):
         "trolleybus": '["route"="trolleybus"]',
         "polish_city": '["name:prefix"="miasto"]',
         "highway": '[highway]',
-        "underground_parking_entrance": '[amenity=parking_entrance][parking=underground]'
+        "underground_parking_entrance": '[amenity=parking_entrance][parking=underground]',
+        'footway': '[highway=footway]',
+        'pedestrian': '[highway=pedestrian]',
+        'steps': '[highway=steps]',
+        'path': '[highway=path]',
+        'track': '[highway=track]',
+        'cycleway': '[highway=cycleway]',
+        'bridleway': '[highway=bridleway]',
+        'motorway': '[highway=motorway]',
+        'motorway_link': '[highway="motorway_link"]',
+        'trunk': '[highway=trunk]',
+        'trunk_link': '[highway="trunk_link"]',
+        'primary': '[highway=primary]',
+        'primary_link': '[highway="primary_link"]',
+        'secondary': '[highway=secondary]',
+        'secondary_link': '[highway="secondary_link"]',
+        'tertiary': '[highway=tertiary]',
+        'tertiary_link': '[highway="tertiary_link"]',
+        'residential': '[highway=residential]',
+        'living_street': '[highway="living_street"]',
+        'service_road': '[highway=service]',
+        'unclassified': '[highway=unclassified]',
+        '_pedestrian_way': 'footway|steps|path|track',
+        '_car_way': 'motorway|motorway_link|primary|primary_link|secondary|secondary_link|tertiary|tertiary_link|residential|living_street|service_road|unclassified',
     }
-    return type_selector_map.get(el_type, None)
+    selector = type_selector_map.get(el_type, None)
+    type_list = selector.split('|')
+    if len(type_list) > 1:
+        for i, val in enumerate(type_list):
+            type_list[i] = type_selector_map.get(val)
+    return type_list
 
 
 if __name__ == "__main__":
-    data = fetch_data_by_id(374172786, 'way', 'node', 'underground_parking_entrance', 'geojson')
-    # print(json.dumps(data, indent=4, sort_keys=True))
+    data = fetch_data_by_id(574812157, 'way', 'way', '_car_way', 'geojson')
+    print(json.dumps(data, indent=4, sort_keys=True))
     r = json.dumps(data, indent=4, sort_keys=True)
     with open("data.json", "w", encoding="utf-8") as f:
         f.write(r)
