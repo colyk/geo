2bb47441591346f7e442d95228fc4f0cf9c9a9ad
colykxer <colykxer@gmail.com>
2019-10-30 18:51:05 +0100

    Add nominatim search

--- backend/geo/osm/utils.py
+++ backend/geo/osm/utils.py
@@ -6,7 +6,17 @@ import requests
 
 def get_info_by_coord(lat: float, lon: float) -> Union[Dict, None]:
     url = "https://nominatim.openstreetmap.org/reverse.php"
-    params = {"lat": lat, "lon": lon, "format": "json"}
+    params: Dict = {"lat": lat, "lon": lon, "format": "json"}
+    res = requests.get(url, params)
+    if res.ok:
+        return res.json()  # If bad request is sent returns dict with 'error' field
+
+    return None
+
+
+def get_info(query) -> Union[Dict, None]:
+    url = "https://nominatim.openstreetmap.org/search/"
+    params = {"q": query, "format": "json"}
     res = requests.get(url, params)
     if res.ok:
         return res.json()  # If bad request is sent returns dict with 'error' field
@@ -16,4 +26,7 @@ def get_info_by_coord(lat: float, lon: float) -> Union[Dict, None]:
 
 if __name__ == "__main__":
     info = get_info_by_coord(51.21824, 22.4233422)
+    print(info)
+
+    info = get_info("Poland, Lublin, nadbystrzycka 38a")
     print(info)
