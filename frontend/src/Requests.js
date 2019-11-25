e5313bf8c17f9f05c3492352c46ec209c6ecbc88
colykxer <colykxer@gmail.com>
2019-11-25 21:43:51 +0100

    F - Add selected on map points description

--- frontend/src/Requests.js
+++ frontend/src/Requests.js
@@ -5,3 +5,9 @@ const BASE_URL = 'http://127.0.0.1:8000';
 export function fetchPath(points = []) {
   return axios.post(`${BASE_URL}/path`, { points });
 }
+
+export function getInfoByCoord(lat, lon) {
+  const url = 'https://nominatim.openstreetmap.org/reverse';
+  const params = { lat, lon, format: 'json' };
+  return axios.get(url, { params });
+}
--- frontend/src/components/PathBuilder.vue
+++ frontend/src/components/PathBuilder.vue
@@ -3,7 +3,7 @@
     <v-container class="grey lighten-5 pa-0" fluid>
       <v-row no-gutters>
         <v-col
-          cols="10"
+          cols="9"
         >
           <v-card
             outlined
@@ -18,7 +18,7 @@
           </v-card>
         </v-col>
         <v-col
-          cols="2"
+          cols="3"
         >
           <v-card
             outlined
@@ -27,13 +27,14 @@
             Selected points:
             <v-list dense>
               <v-list-item
-                v-for="(point, index) in selectedPoints"
+                v-for="(point, index) in pointsInfo"
                 :key="index"
                 @contextmenu.prevent="$refs.menu.open($event, {point, index})"
               >
                 <v-list-item-content>
-                  <v-list-item-title>Lat: {{ point[0] }}</v-list-item-title>
-                  <v-list-item-title>Lng: {{ point[1] }}</v-list-item-title>
+                  <v-list-item-title>{{ point.name }}</v-list-item-title>
+                  <v-list-item-subtitle >Lat: {{ point.lat }}</v-list-item-subtitle>
+                  <v-list-item-subtitle >Lon: {{ point.lon }}</v-list-item-subtitle>
                 </v-list-item-content>
               </v-list-item>
               <v-list-item v-if="pathLength">
@@ -63,7 +64,7 @@
 
 import { VueContext } from 'vue-context';
 import Map from './Map.vue';
-import { fetchPath } from '../Requests';
+import { fetchPath, getInfoByCoord } from '../Requests';
 import { hasCoords } from '../utils';
 
 function degreesToRadians(degrees) {
@@ -96,6 +97,7 @@ export default {
   data() {
     return {
       selectedPoints: [],
+      pointsInfo: [],
       lineCoords: [],
       clickedOnPoint: null,
       geojson: null,
@@ -106,6 +108,7 @@ export default {
     onClick(type) {
       if (type === 'remove') {
         this.selectedPoints.splice(this.clickedOnPoint.index, 1);
+        this.pointsInfo.splice(this.clickedOnPoint.index, 1);
         this.lineCoords = [];
         this.pathLength = null;
         this.requestPath();
@@ -118,7 +121,9 @@ export default {
     },
     onRightClick({ lat, lng }) {
       if (!hasCoords(this.selectedPoints, [lat, lng])) {
+        this.addPointInfo(lat, lng);
         this.selectedPoints.push([lat, lng]);
+
         this.requestPath();
       }
     },
@@ -134,6 +139,19 @@ export default {
           })
           .catch((e) => { console.log(e); });
       }
+    },
+    addPointInfo(lat, lon) {
+      getInfoByCoord(lat, lon)
+        .then(({ data }) => {
+          const info = { lat: Number(data.lat).toFixed(7), lon: Number(data.lon).toFixed(7), name: data.display_name };
+          console.log(info);
+          this.pointsInfo.push(info);
+        })
+        .catch((e) => {
+          const info = { lat, lon, name: '-' };
+          this.pointsInfo.push(info);
+          console.log(e);
+        });
     },
 
   },
